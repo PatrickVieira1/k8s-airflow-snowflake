bbaad3afe72ab97298845504116ac6f717ccb81a
tick <tick@Ubuntu.myguest.virtualbox.org>
2024-03-05 20:39:38 -0300

    adding connector

--- src/connect_db.py
+++ src/connect_db.py
@@ -1,3 +1,16 @@
 import os
+import snowflake.connector
 
-print('bbb')
+con = snowflake.connector.connect(
+    user=os.getenv('USERNAME'),
+    password=os.getenv('PASSWORD') + '>&B~HG',
+    account=os.getenv('ACCOUNT'),
+    warehouse='COMPUTH_WH',
+    database=os.getenv('DATABASE'),
+    schema=os.getenv('SCHEMA'),
+)
+
+con.cursor().execute('CREATE TABLE IF NOT EXISTS SAMPLE_DATA')
+
+
+
